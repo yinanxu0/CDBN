946806fde0da13ecfb044643a0702418c1f54385
xuyinan <yinanxu@umich.edu>
2015-12-15 05:11:28 -0500

    modified layers

--- test_dbn.py
+++ test_dbn.py
@@ -2,7 +2,6 @@
 
 import cPickle
 import os
-import sys
 import timeit
 import numpy
 import theano
@@ -11,13 +10,29 @@ import load_CIFAR as LF
 from DBN import DBN
 from load_data import load_data
 
-DEBUG_SET = 1
+DEBUG_SET = 0
 
 def test_DBN(finetune_lr=0.05, pretraining_epochs=20, pretrain_lr=0.01,
             k=1, training_epochs=1000, batch_size=10,DEBUG=0):
 
     # datasets = LF.load_cifar()
     # IMAGE_SIZE = 32
+    # if DEBUG:
+    #     N1 = 400
+    #     N2 = 600
+    # else:
+    #     N1 = 40000
+    #     N2 = 50000
+    # x, y = datasets[0]
+    # train_set_x = x[:N1]
+    # train_set_y = y[:N1]
+    # valid_set_x = x[N1:N2]
+    # valid_set_y = y[N1:N2]
+    # test_set_x, test_set_y = datasets[1]
+    # datasets = [(train_set_x, train_set_y),
+    #             (valid_set_x, valid_set_y),
+    #             (test_set_x, test_set_y)]
+
     datasets = load_data(DEBUG=DEBUG)
     IMAGE_SIZE = 28
 
@@ -69,9 +84,7 @@ def test_DBN(finetune_lr=0.05, pretraining_epochs=20, pretrain_lr=0.01,
             print 'Pre-training layer %i, epoch %d, cost %.4f' % (i+1, epoch+1, numpy.mean(c))
 
     end_time = timeit.default_timer()
-    print >> sys.stderr, ('The pretraining code for file ' +
-                          os.path.split(__file__)[1] +
-                          ' ran for %.2fm' % ((end_time - start_time) / 60.))
+    print 'The pretraining for DBN ran for %.2fm' % ((end_time-start_time)/60.))
     ########################
     # FINETUNING THE MODEL #
     ########################
@@ -85,86 +98,44 @@ def test_DBN(finetune_lr=0.05, pretraining_epochs=20, pretrain_lr=0.01,
     )
 
     print '... finetuning the model'
-    # early-stopping parameters
-    patience = 4 * n_train_batches  # look as this many examples regardless
-    patience_increase = 2.    # wait this much longer when a new best is
-                              # found
-    improvement_threshold = 0.995  # a relative improvement of this much is
-                                   # considered significant
-    validation_frequency = min(n_train_batches, patience / 2)
-                                  # go through this many minibatches before
-                                  # checking the network on the validation
-                                  # set; in this case we check every epoch
-
-    best_validation_loss = numpy.inf
+    best_valid = numpy.inf
     test_score = 0.
     start_time = timeit.default_timer()
-
-    done_looping = False
     epoch = 0
-
     while (epoch < training_epochs) and (not done_looping):
         epoch = epoch + 1
         for minibatch_index in xrange(n_train_batches):
 
             minibatch_avg_cost = train_fn(minibatch_index)
-            iter = (epoch - 1) * n_train_batches + minibatch_index
 
-            if (iter + 1) % validation_frequency == 0:
+            if n_train_batches - minibatch_index == 1:
 
                 validation_losses = [validate_fn(i) for i in xrange(n_valid_batches)]
-                this_validation_loss = numpy.mean(validation_losses)
-                print(
-                    'epoch %i, minibatch %i/%i, validation error %.2f %%'
-                    % (
-                        epoch,
-                        minibatch_index + 1,
-                        n_train_batches,
-                        this_validation_loss * 100.
-                    )
-                )
+                valid_score = 1-numpy.mean(validation_losses)
+                print('epoch %i, minibatch %i/%i, validation error %.2f %%'%(epoch, valid_score * 100.))
 
                 # if we got the best validation score until now
-                if this_validation_loss < best_validation_loss:
-
-                    #improve patience if loss improvement is good enough
-                    if (
-                        this_validation_loss < best_validation_loss *
-                        improvement_threshold
-                    ):
-                        patience = max(patience, iter * patience_increase)
+                if valid_score > best_valid:
 
                     # save best validation score and iteration number
-                    best_validation_loss = this_validation_loss
-                    best_iter = iter
+                    best_valid = valid_score
 
                     # test it on the test set
                     test_losses = [test_fn(i) for i in xrange(n_test_batches)]
                     test_score = 1-numpy.mean(test_losses)
-                    print(('     epoch %i, minibatch %i/%i, test error of '
-                           'best model %.2f %%') %
-                          (epoch, minibatch_index + 1, n_train_batches,
-                           test_score * 100.))
+                    print(('     epoch %i, test error of best model %.2f %%')%(epoch, test_score * 100.))
 
                     # save the best model
                     f = file('best_model_dbn.pkl', 'w')
                     cPickle.dump(dbn, f, protocol=cPickle.HIGHEST_PROTOCOL)
                     f.close()
 
-            if patience <= iter:
-                done_looping = True
-                break
-
     end_time = timeit.default_timer()
-    print(
-        ('Optimization complete with best validation score of %.2f %%, '
-            'with test performance %.2f %%') 
-        %((1-best_validation_loss)*100., test_score * 100.)
+    print(('Optimization with best validation score of %.2f %%, with test performance %.2f %%') 
+        %(best_valid*100., test_score * 100.)
     )
-    print >> sys.stderr, ('The fine tuning code for file ' +
-                          os.path.split(__file__)[1] +
-                          ' ran for %.2fm' % ((end_time - start_time)
-                                              / 60.))
+    print 'The finetune for DBN ran for %.2fm' % ((end_time-start_time)/60.))
 
 if __name__ == '__main__':
     test_DBN(DEBUG=DEBUG_SET)
+
